5402c5c6e184408af7d5bc1d80af4576250634db
Matinhas <matheusvictor007@gmail.com>
2024-03-25 20:41:44 -0300

    .

--- myprojeto/screens/contatoScreen.js
+++ myprojeto/screens/contatoScreen.js
@@ -19,7 +19,7 @@ const firebaseConfig = {
 //Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-
+//Logout
 const ContatoScreen = ({ route, navigation }) => {
 
     const handleLogout = () => {
